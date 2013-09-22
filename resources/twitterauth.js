2f06c05973c1272adac375db6785c7261ce611d4
Joey Guerra <joey@joeyguerra.com>
2013-09-21 20:51:02 -0500

    troubleshooting. remove twitter callback auth

--- resources/twitterauth.js
+++ resources/twitterauth.js
@@ -3,7 +3,9 @@ var passport = require('passport');
 module.exports = function (app){
 	var self = new Resource();
 	app.get('/auth/twitter', passport.authenticate('twitter'));
-	app.get("/auth/twitter/callback", passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/' }));
+	app.get("/auth/twitter/callback", function(req, resp, next){
+		resp.send(req);
+	});
 	app.get("/home", function(req, resp, next){
 		resp.send(req.user);
 	});
