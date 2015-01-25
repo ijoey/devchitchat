b7fe8c0967d07beafb0e28378b506e967ce29fb8
Joey Guerra <joey@joeyguerra.com>
2015-01-25 06:07:14 -0600

    removing unused files.

--- env.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var data = fs.readFileSync('dev.env', 'utf8');
-var env = {};
-data.split(/\n/).forEach(function(line){
-	var match = line.match(/^([^=:]+?)[=\:](.*)/);
-	if(match[0]) env[match[1]] = match[2];
-});
-module.exports = env;
