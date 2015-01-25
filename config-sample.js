abaaa5aa3e764261da4956c8f980bace6705195b
Joey Guerra <joey@joeyguerra.com>
2015-01-25 05:47:08 -0600

    adding sample config file.

---
+++ config-sample.js
@@ -0,0 +1,28 @@
+module.exports = {
+	dataPath: __dirname + '/data',
+	port: 5000,
+    site: {
+      title: 'devchitchat'
+    },
+	theme: 'default',
+	cookie: {
+		key: 'devchitchat',
+		secret: 'some secret password for signed cookies'
+    },
+	secret: "Milo of Croton carried the calf as it grew into a bull.",
+	hubot:{
+		token: ""
+	},
+	passport: {
+		github: {
+			clientID: '',
+			clientSecret: '',
+			callbackURL: 'http://localhost:5000/github/callback'		
+		},
+		twitter: {
+			consumerKey: "",
+			consumerSecret: "",
+			callbackUrl: "http://localhost:5000/twitter/callback"
+		}
+	}
+};
