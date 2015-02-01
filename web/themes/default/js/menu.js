242af205188373ce3b007d88e0ab71569651ab18
Joey Guerra <joey@joeyguerra.com>
2015-02-01 13:31:01 -0600

    refactored menu button
    to be created via Javascript instead of putting it in the HTML because
    I didn’t want it to show up on the home page.

--- web/themes/default/js/menu.js
+++ web/themes/default/js/menu.js
@@ -1,17 +1,19 @@
 (function(n, win){
 	n.CreateMenu = function(container, model, delegate){
 		var self = {
-			container: container
-			, delegate: delegate
-			, model: model
-			, show: function(){
+			container: container,
+			delegate: delegate,
+			model: model,
+			show: function(){
 				this.container.style['left'] = '0px';
-			}
-			, hide: function(){
+			},
+			hide: function(){
 				this.container.style['left'] = '-100px';
-			}
-			, handleEvent: function(e){
-				if(!e.target.href) return;
+			},
+			handleEvent: function(e){
+				if(!e.target.href){
+					return;
+				}
 				e.preventDefault();
 				this.delegate.wasSelected(e.target);
 			}
@@ -19,8 +21,8 @@
 		Object.defineProperty(self, 'isHidden', {
 			get: function(){
 				return parseInt(self.container.style['left'].replace('px', '')) !== 0;;
-			}
-			, enumerable: true
+			},
+			enumerable: true
 		});
 		self.container.addEventListener('click', self, true);
 		return self;
@@ -28,27 +30,30 @@
 	
 	n.CreateMenuButton = function(container, model, delegate){
 		var self = {
-			container: container
-			, model: model
-			, delegate: delegate
-			, toggle: function(){
-				if(this.isOpened) this.close();
-				else this.open();
-			}
-			, close: function(){
+			container: container,
+			model: model,
+			delegate: delegate,
+			toggle: function(){
+				if(this.isOpened){
+					this.close();
+				} else {
+					this.open();
+				}
+			},
+			close: function(){
 				this.container.style['left'] = this.defaultLeft + 'px';
 				this.delegate.shouldClose(this);
-			}
-			, open: function(){
+			},
+			open: function(){
 				this.container.style['left'] = '100px';
 				this.delegate.shouldOpen(this);
-			}
-			, handleEvent: function(e){
+			},
+			handleEvent: function(e){
 				if(this[e.type]){
 					this[e.type](e);
 				}
-			}
-			, click: function(e){
+			},
+			click: function(e){
 				this.toggle();
 			}
 		};
@@ -65,19 +70,19 @@
 		
 	n.CreateDocument = function(container, model, delegate){
 		var self = {
-			container: container
-			, model: model
-			, delegate: delegate
-			, slideRight: function(){
+			container: container,
+			model: model,
+			delegate: delegate,
+			slideRight: function(){
 				this.container.style['marginLeft'] = '100px';
-			}
-			, slideLeft: function(){
+			},
+			slideLeft: function(){
 				this.container.style['marginLeft'] = 'auto';				
-			}
-			, menuHasOpened: function(publisher, view){
+			},
+			menuHasOpened: function(publisher, view){
 				this.slideRight();
-			}
-			, menuHasClosed: function(publisher, view){
+			},
+			menuHasClosed: function(publisher, view){
 				this.slideLeft();
 			}
 		};
@@ -88,20 +93,20 @@
 
 	n.CreateUrlListener = function(model, delegate){
 		var self = {
-			delegate: delegate
-			, model: model
-			, update: function(key, old, v){
+			delegate: delegate,
+			model: model,
+			update: function(key, old, v){
 				var xhr = new XMLHttpRequest();
 				var self = this;
 				var url = this.addExtension(v, 'phtml');
 				xhr.open("GET", url, true);
 				xhr.onload = function(e){self.onload(e);};
 				xhr.send();
-			}
-			, onload: function(e){
+			},
+			onload: function(e){
 				this.delegate.pageWasRequested(this.model, e.target.responseText);
-			}
-			, addExtension: function(url, ext){
+			},
+			addExtension: function(url, ext){
 				var parts = url.split('/');
 				var last = parts[parts.length-1];
 				if(last.length === 0) url = 'index';
@@ -120,8 +125,8 @@
 		var menu = new n.Observable({});
 		var page = new n.Observable({url: window.location.href});
 		var self = {
-			views: []
-			, wasSelected: function(link){
+			views: [],
+			wasSelected: function(link){
 				n.NotificationCenter.publish('pageWasSelected', this, page);
 				return window.location = link.href;
 
@@ -137,24 +142,24 @@
 				window.history.pushState(null, link.title, link.href);
 				page.url = link.href;
 				n.NotificationCenter.publish('pageWasSelected', this, page);
-			}
-			, pageWasRequested: function(p, response){
+			},
+			pageWasRequested: function(p, response){
 				var main = document.getElementById('main');
 				main.innerHTML = response;
 				var background = main.querySelector('#background');
 				if(background){
 					document.body.style['backgroundImage'] = 'url("' + background.value + '")';	
 				}
-			}
-			, shouldOpen: function(button){
+			},
+			shouldOpen: function(button){
 				this.views.forEach(function(v){
 					if(v.container.id === 'menu'){
 						v.show();
 						n.NotificationCenter.publish('menuHasOpened', this, v);
 					}
 				}.bind(this));
-			}
-			, shouldClose: function(button){
+			},
+			shouldClose: function(button){
 				this.views.forEach(function(v){
 					if(v.container.id === 'menu'){
 						v.hide();						
@@ -163,14 +168,14 @@
 				}.bind(this));
 			}
 		};
-		//var listener = n.CreateUrlListener(page, self);
-		self.views.push(n.CreateMenuButton(document.getElementById('menuButton'), menu, self));
+		var menuButton = document.createElement('button');
+		menuButton.id = 'menuButton';
+		menuButton.className = 'menu glyphicon glyphicon-align-justify';
+		menuButton.innerHTML = 'Menu';
+		document.body.appendChild(menuButton);
+		self.views.push(n.CreateMenuButton(menuButton, menu, self));
 		self.views.push(n.CreateMenu(document.getElementById('menu'), menu, self));
-		self.views.push(n.CreateDocument(document.getElementById('main'), page, self));
-		// window.addEventListener('popstate', function(e){
-// 			console.log('popstate', e);
-// 		}, true);
-		
+		self.views.push(n.CreateDocument(document.getElementById('main'), page, self));		
 		return self;
 	})(win);
 })(MM, window);
